70f9b6cd23a671d915d1a9351e22998515873b1d
FelipeSantanaC <felipecabocloo@gmail.com>
2024-05-01 02:27:04 -0300

    feat: add initial header component

--- fsw-foods/app/page.tsx
+++ fsw-foods/app/page.tsx
@@ -1,9 +1,7 @@
-const name = "Felipe";
+import Header from "./_components/header";
 
 export default function Home() {
   return (
-    <div className="bg-red-500 py-5 mx-6">
-      <h1 className="p-2">Hello Tailwind!</h1>
-    </div>
+    <Header/>
   );
 }
